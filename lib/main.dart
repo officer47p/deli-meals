d805f8896ede3dfe970dd0d0ace73178ebd358b3
Parsa Hosseini <officer47p@gmail.com>
2020-03-28 14:54:15 +0430

    configured the grid view, added the dummy data and created the category model

---
+++ lib/main.dart
@@ -0,0 +1,39 @@
+import "package:flutter/material.dart";
+
+import './categories_screen.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: "DeliMeals",
+      theme: ThemeData(
+        primaryColor: Colors.blue,
+      ),
+      home: MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("DeliMeals"),
+      ),
+      body: Center(
+        child: Text("This is my app"),
+      ),
+    );
+  }
+}
