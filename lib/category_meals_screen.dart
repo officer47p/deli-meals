5b188a3f9c783e7e51f52ceda8a1c583b8df1093
Parsa Hosseini <officer47p@gmail.com>
2020-03-31 00:29:33 +0430

    filtering the meals related to the category page that we are in, and showing a simple text in a ListView widget

--- lib/category_meals_screen.dart
+++ lib/category_meals_screen.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 
+import './dummy_data.dart';
+
 class CategoryMealsScreen extends StatelessWidget {
   static const String name = "/category-meals";
   // final String categoryId;
@@ -12,12 +14,20 @@ class CategoryMealsScreen extends StatelessWidget {
     final passedArgs = (ModalRoute.of(context).settings.arguments as Map);
     final categoryTitle = passedArgs["title"];
     final categoryId = passedArgs["id"];
+    final meals = DUMMY_MEALS.where((item) {
+      return item.categories.contains(categoryId);
+    }).toList();
     return Scaffold(
       appBar: AppBar(
         title: Text(categoryTitle),
       ),
-      body: Center(
-        child: Text("The Recipes For The Category!"),
+      body: ListView.builder(
+        itemBuilder: (ctx, i) {
+          return Container(
+            child: Text(meals[i].title),
+          );
+        },
+        itemCount: meals.length,
       ),
     );
   }
