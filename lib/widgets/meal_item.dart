84100526eab84ad920c1df860524f0316e2e9680
Parsa Hosseini <officer47p@gmail.com>
2020-04-01 23:46:35 +0430

    completed the meal item widget

--- lib/widgets/meal_item.dart
+++ lib/widgets/meal_item.dart
@@ -6,6 +6,18 @@ class MealItem extends StatelessWidget {
   final Meal meal;
   MealItem({@required this.meal});
 
+  String get complexityString {
+    if (meal.complexity == Complexity.Simple) return "Simple";
+    if (meal.complexity == Complexity.Challenging) return "Challenging";
+    if (meal.complexity == Complexity.Hard) return "Hard";
+  }
+
+  String get affordabilityString {
+    if (meal.affordability == Affordability.Affordable) return "Affordable";
+    if (meal.affordability == Affordability.Pricey) return "Pricey";
+    if (meal.affordability == Affordability.Luxurious) return "Luxurious";
+  }
+
   @override
   Widget build(BuildContext context) {
     return InkWell(
@@ -30,8 +42,58 @@ class MealItem extends StatelessWidget {
                     width: double.infinity,
                     fit: BoxFit.cover,
                   ),
+                ),
+                Positioned(
+                  bottom: 10,
+                  right: 20,
+                  child: Container(
+                    padding: EdgeInsets.symmetric(vertical: 5, horizontal: 15),
+                    color: Colors.black54,
+                    width: MediaQuery.of(context).size.width * 0.7,
+                    child: Text(
+                      meal.title,
+                      style: TextStyle(fontSize: 26, color: Colors.white),
+                      softWrap: true,
+                      overflow: TextOverflow.fade,
+                    ),
+                  ),
                 )
               ],
+            ),
+            Padding(
+              padding: const EdgeInsets.all(20.0),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceAround,
+                children: <Widget>[
+                  Row(
+                    children: <Widget>[
+                      Icon(Icons.schedule),
+                      SizedBox(
+                        width: 6,
+                      ),
+                      Text("${meal.duration} min")
+                    ],
+                  ),
+                  Row(
+                    children: <Widget>[
+                      Icon(Icons.work),
+                      SizedBox(
+                        width: 6,
+                      ),
+                      Text("${complexityString}")
+                    ],
+                  ),
+                  Row(
+                    children: <Widget>[
+                      Icon(Icons.attach_money),
+                      SizedBox(
+                        width: 6,
+                      ),
+                      Text("${affordabilityString}")
+                    ],
+                  )
+                ],
+              ),
             )
           ],
         ),
